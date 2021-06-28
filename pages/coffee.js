ad0a34b987e979a7e1546bcd93d140070901b22e
sammyjoyce <samjoyce@me.com>
2021-06-28 12:07:18 +1000

    coffee popup

--- pages/coffee.js
+++ pages/coffee.js
@@ -1,35 +1,36 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Link from "next/link";
+import { toaster } from "evergreen-ui";
+import Footer from "@components/Footer";
+import Nav from "@components/Nav";
 
-import Nav from '@components/Nav'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import ContactForm from '@components/ContactForm'
-
-export default function Home() {
+export default function coffee() {
   return (
     <div className="container">
       <Head>
-        <title>coLab Booking</title>
+        <title>coLab Coffee Order</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
       <main>
         <div>
-          <Link href="/coffee">
-            <button>Coffee Order</button>
-          </Link>
-          <Link href="/room">
-            <button>Meeting Room Booking</button>
-          </Link>
-          <Link href="/2908">
-            <button>2908 Access</button>
-          </Link>
+          <button
+            onClick={() =>
+              toaster.success(
+                // "If you had to ask...",
+                <img src="https://media.giphy.com/media/26xBsuyfTVScq7UxW/source.gif" />
+              )
+            }
+          >
+            Have I had too much coffee?
+          </button>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLScIHhz1SVqtFuYjuBAyAYKTvgS2fa-7uoECTiUkLs5-0ceShA/viewform">
+            <button>Just let me order</button>
+          </a>
         </div>
       </main>
 
       <Footer />
-
       <style jsx>{`
         .container {
           display: flex;
@@ -67,28 +68,6 @@ export default function Home() {
             margin: 3%;
             grid-template-columns: 1fr;
           }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        @import url("https://use.typekit.net/uuv2vhl.css");
-
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: jaf-domus, -apple-system, BlinkMacSystemFont, Segoe UI,
-            Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-            Helvetica Neue, sans-serif;
-          font-weight: 300;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        .title {
-          font-size: 1.6em;
         }
       `}</style>
     </div>
